11b88f098947a79cdb6e11dd7429e0ab5d2c9214
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-10-26 16:49:09 +0700

    nav component , link

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import style from "../styles/Layout.module.css";
+import Nav from "./Nav";
 
 const Layout = ({ children }) => {
   return (
-    <div className={style.container}>
-      <main className={style.main}>{children}</main>
+    <div>
+      <Nav />
+      <div className={style.container}>
+        <main className={style.main}>{children}</main>
+      </div>
     </div>
   );
 };
