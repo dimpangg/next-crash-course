7d5fc7808d875d3d16280779566f0782244716fb
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-10-28 14:53:57 +0700

    nested routing

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -3,7 +3,7 @@ import style from "../styles/Article.module.css";
 
 const ArticleItem = ({ article }) => {
   return (
-    <Link href="/href/[id]" as={`/article/${article.id}`}>
+    <Link href="/article/[id]" as={`/article/${article.id}`}>
       <a className={style.card}>
         <h3>{article.title} &rarr;</h3>
         <p>{article.body}</p>
