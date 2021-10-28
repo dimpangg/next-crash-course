322236e60718c0d00e55aa1ce2ef6ee2db807ce8
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-10-28 23:01:56 +0700

    getstaticpaths

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -5,7 +5,7 @@ const ArticleList = ({ articles }) => {
   return (
     <div className={style.grid}>
       {articles.map((article) => (
-        <ArticleItem article={article} />
+        <ArticleItem key={article.id} article={article} />
       ))}
     </div>
   );
