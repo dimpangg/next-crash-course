1833169085970956f42b4760b9aaa6b3eaf077ca
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-10-28 15:02:54 +0700

    getserversideprops

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,9 +1,29 @@
+import Link from "next/link";
 import { useRouter } from "next/dist/client/router";
 
-const article = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  return <div>This is article {id}</div>;
+const article = ({ article }) => {
+  // const router = useRouter();
+  // const { id } = router.query;
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+    </>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
+  return {
+    props: {
+      article,
+    },
+  };
 };
 
 export default article;
